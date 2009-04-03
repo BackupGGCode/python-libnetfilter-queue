6053ba1865f3164210c5dac1bc04dce8b722af6a
alopezluksenberg <alopezluksenberg@0075cb7a-1d29-11de-bcda-2bb9902f8029>
2009-04-03 03:30:47 +0000

    Code clean. Documented.

--- libnetfilter-ctypes.py
+++ libnetfilter-ctypes.py
@@ -4,13 +4,9 @@ import socket
 import os
 
 #TODO:
-#como hago un typedef int lala, esto lo necesito para definir nfq_callback
-#test open_nfnl function
-#test get_packet_hw
-#como hago para hacer esto: u_int8_t hw_addr[8]; en la funcion nfqnl_msg_packet_hw
-#
-
-
+#     All test functions
+#     Explain all verdicts
+#     Fix hw_addr type
 
 #load library
 netfilter = ctypes.cdll.LoadLibrary(utils.find_library('netfilter_queue'))
@@ -29,7 +25,7 @@ class nfq_data(ctypes.Structure):
 class nfqnl_msg_packet_hw(ctypes.Structure):
     _fields_ = [("hw_addrlen", ctypes.c_uint16),
                 ("_pad", ctypes.c_uint16),
-                #TODO
+                #############################
                 ("hw_addr", ctypes.c_void_p)]
 
 
@@ -73,7 +69,6 @@ class nlif_handle(ctypes.Structure):
                 ('ifdel_handler', ctypes.c_void_p)]
 
 
-
 time_t = ctypes.c_long
 suseconds_t = ctypes.c_long
 
@@ -102,7 +97,6 @@ class nfq_data(ctypes.Structure):
 ##################################################################
 # Responses from hook functions.
 ##################################################################
-# http://www.comp.nus.edu.sg/~cs4236/readings/out/html/netfilter_8h-source.html#l00016
 NF_DROP, NF_ACCEPT, NF_STOLEN = 0, 1, 2
 NF_QUEUE, NF_REPEAT, NF_STOP= 3, 4, 5
 NF_MAX_VERDICT = NF_STOP
@@ -115,135 +109,211 @@ NFQNL_COPY_NONE, NFQNL_COPY_META, NFQNL_COPY_PACKET = 0, 1, 2
 
 
 #define functions. set argument and response values
+
+#Description:
+#            Return netfilter netlink handler.
 nfnlh = netfilter.nfq_nfnlh
 nfnlh.restype = ctypes.POINTER(nfnl_handle)
 nfnlh.argtypes = ctypes.POINTER(nfq_handle),
 ########
 
+#Description:
+#            Return a file descriptor for the netlink connection associated with the
+#            given queue connection handle.
 nfq_fd = netfilter.nfnl_fd
 nfq_fd.restype = ctypes.c_int
 nfq_fd.argtypes = ctypes.POINTER(nfnl_handle),
 ########
 
-nfq_open = netfilter.nfq_open
-nfq_open.restype = ctypes.POINTER(nfq_handle)
+#Description:
+#            This function obtains a netfilter queue connection handle
+open_queue = netfilter.nfq_open
+open_queue.restype = ctypes.POINTER(nfq_handle)
 ########
 
-open_nfnl = netfilter.nfq_open_nfnl
-open_nfnl.restype = ctypes.POINTER(nfq_handle)
-open_nfnl.argtypes = ctypes.POINTER(nfnl_handle), 
+#Description:
+#            Open a nfqueue handler from a existing nfnetlink handler.
+#            Not implemented in this wrapper. 
+#open_nfnl = netfilter.nfq_open_nfnl
+#open_nfnl.restype = ctypes.POINTER(nfq_handle)
+#open_nfnl.argtypes = ctypes.POINTER(nfnl_handle), 
 ########
 
-nfq_close = netfilter.nfq_close
-nfq_close.restype = ctypes.c_int
-nfq_close.argtypes = ctypes.POINTER(nfq_handle),
+#Description:
+#            This function closes the nfqueue handler and free associated resources.           
+close_queue = netfilter.nfq_close
+close_queue.restype = ctypes.c_int
+close_queue.argtypes = ctypes.POINTER(nfq_handle),
 ########
 
+#Description:
+#            Bind a nfqueue handler to a given protocol family.
 bind_pf = netfilter.nfq_bind_pf
 bind_pf.restype = ctypes.c_int
 bind_pf.argtypes = ctypes.POINTER(nfq_handle), ctypes.c_uint16 
 ########
 
+#Description:
+#            Unbind nfqueue handler from a protocol family.
 unbind_pf = netfilter.nfq_unbind_pf
 unbind_pf.restype = ctypes.c_int
 unbind_pf.argtypes = ctypes.POINTER(nfq_handle), ctypes.c_uint16
 ########
 
+#Description:
+#            Creates a new queue handle, and returns it.
 create_queue = netfilter.nfq_create_queue
 create_queue.restype = ctypes.POINTER(nfq_q_handle)
 create_queue.argtypes = ctypes.POINTER(nfq_handle), ctypes.c_uint16, ctypes.c_void_p, ctypes.c_void_p
 ########
 
+#Description:
+#            Removes the binding for the specified queue handle.
 destroy_queue = netfilter.nfq_destroy_queue
 destroy_queue.restype = ctypes.c_int
 destroy_queue.argtypes = ctypes.POINTER(nfq_q_handle),
 ########
 
+#Description:
+#            Triggers an associated callback for the given packet received from the
+#            queue.
 handle_packet = netfilter.nfq_handle_packet
 handle_packet.restype = ctypes.c_int
 handle_packet.argtypes = ctypes.POINTER(nfq_handle), ctypes.c_char_p, ctypes.c_int
 ########
 
+#Description:
+#            Sets the amount of data to be copied to userspace for each packet queued
+#            to the given queue.
+#         
+#            NFQNL_COPY_NONE - do not copy any data
+#            NFQNL_COPY_META - copy only packet metadata
+#            NFQNL_COPY_PACKET - copy entire packet
 set_mode = netfilter.nfq_set_mode
 set_mode.restype = ctypes.c_int
 set_mode.argtypes = ctypes.POINTER(nfq_q_handle), ctypes.c_uint8, ctypes.c_uint
 ########
 
+#Description:
+#            Sets the size of the queue in kernel. This fixes the maximum number
+#            of packets the kernel will store before internally before dropping
+#            upcoming packets.
 set_queue_maxlen = netfilter.nfq_set_queue_maxlen
 set_queue_maxlen.restype = ctypes.c_int
 set_queue_maxlen.argtypes = ctypes.POINTER(nfq_q_handle), ctypes.c_uint32
 ########
 
+#Description:
+#            Notifies netfilter of the userspace verdict for the given packet. Every
+#            queued packet _must_ have a verdict specified by userspace, either by
+#            calling this function, or by calling the nfq_set_verdict_mark() function.
+#            NF_DROP - Drop packet
+#            NF_ACCEPT - Accept packet
+#            NF_STOLEN -
+#            NF_QUEUE - Enqueue the packet
+#            NF_REPEAT - Handle the same packet
+#            NF_STOP - 
+#            NF_MAX_VERDICT -
 set_verdict = netfilter.nfq_set_verdict
 set_verdict.restype = ctypes.c_int
 set_verdict.argtypes = ctypes.POINTER(nfq_q_handle), ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_char_p
 ########
 
+#Description:
+#            Like set_verdict, but you can set the mark.
 set_verdict_mark = netfilter.nfq_set_verdict_mark
 set_verdict_mark.restype = ctypes.c_int
 set_verdict_mark.argtypes = ctypes.POINTER(nfq_q_handle), ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_char_p
 ########
 
+#Description:
+#            Return the metaheader that wraps the packet.
 get_msg_packet_hdr = netfilter.nfq_get_msg_packet_hdr
 get_msg_packet_hdr.restype = ctypes.POINTER(nfqnl_msg_packet_hdr)
 get_msg_packet_hdr.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#            Return the netfilter mark currently assigned to the given queued packet.
 get_nfmark = netfilter.nfq_get_nfmark
 get_nfmark.restype = ctypes.c_uint32
 get_nfmark.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#            Retrieves the received timestamp when the given queued packet.
 get_timestamp = netfilter.nfq_get_timestamp
 get_timestamp.restype = ctypes.c_int
 get_timestamp.argtypes = ctypes.POINTER(nfq_data), ctypes.POINTER(timeval)
 ########
 
+#Description:
+#            Return the index of the device the queued packet was received via.  If the
+#            returned index is 0, the packet was locally generated or the input
+#            interface is not known.
 get_indev = netfilter.nfq_get_indev
 get_indev.restype = ctypes.c_uint32
 get_indev.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#            Return the index of the physical device the queued packet was received via.
+#            If the returned index is 0, the packet was locally generated or the
+#            physical input interface is no longer known.
 get_physindev = netfilter.nfq_get_physindev
 get_physindev.restype = ctypes.c_uint32
 get_physindev.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#           Return the index of the device the queued packet will be sent out.
 get_outdev = netfilter.nfq_get_outdev
 get_outdev.restype = ctypes.c_uint32
 get_outdev.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#           Return The index of physical interface that the packet output will be routed out
 get_physoutdev = netfilter.nfq_get_physoutdev
 get_physoutdev.restype = ctypes.c_uint32
 get_physoutdev.argtypes = ctypes.POINTER(nfq_data),
 ########
 
-get_indev_name = netfilter.nfq_get_indev_name
-get_indev_name.restype = ctypes.c_int
-get_indev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
+##################################################################    
+#Not implemented yet.
+##################################################################    
+
+#get_indev_name = netfilter.nfq_get_indev_name
+#get_indev_name.restype = ctypes.c_int
+#get_indev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
 ########
 
-get_physindev_name = netfilter.nfq_get_physindev_name
-get_physindev_name.restype = ctypes.c_int
-get_physindev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
+#get_physindev_name = netfilter.nfq_get_physindev_name
+#get_physindev_name.restype = ctypes.c_int
+#get_physindev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
 ########
 
-get_outdev_name = netfilter.nfq_get_outdev_name
-get_outdev_name.restype = ctypes.c_int
-get_outdev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
+#get_outdev_name = netfilter.nfq_get_outdev_name
+#get_outdev_name.restype = ctypes.c_int
+#get_outdev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
 ########
 
-get_physoutdev_name = netfilter.nfq_get_physoutdev_name
-get_physoutdev_name.restype = ctypes.c_int
-get_physoutdev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
+#get_physoutdev_name = netfilter.nfq_get_physoutdev_name
+#get_physoutdev_name.restype = ctypes.c_int
+#get_physoutdev_name.argtypes = ctypes.POINTER(nlif_handle), ctypes.POINTER(nfq_data), ctypes.c_char_p
 ########
 
+##################################################################    
+
+#Description:
+#           Retrieves the hardware address associated with the given queued packet.
 get_packet_hw = netfilter.nfq_get_packet_hw
 get_packet_hw.restype = ctypes.POINTER(nfqnl_msg_packet_hw)
 get_packet_hw.argtypes = ctypes.POINTER(nfq_data),
 ########
 
+#Description:
+#           Retrieve the payload for a queued packet.
 get_payload = netfilter.nfq_get_payload
 get_payload.restype = ctypes.c_int
 get_payload.argtypes = ctypes.POINTER(nfq_data), ctypes.POINTER(ctypes.c_char_p)
@@ -251,7 +321,7 @@ get_payload.argtypes = ctypes.POINTER(nfq_data), ctypes.POINTER(ctypes.c_char_p)
 
 HANDLER = ctypes.CFUNCTYPE(
                            #(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data)
-                           ctypes.c_int, *(ctypes.POINTER(nfq_q_handle), ctypes.c_void_p, ctypes.POINTER(nfq_data), ctypes.c_void_p)
+                           None, *(ctypes.POINTER(nfq_q_handle), ctypes.c_void_p, ctypes.POINTER(nfq_data), ctypes.c_void_p)
         )
 
 
@@ -270,9 +340,6 @@ def py_handler(uno, dos, tres, cuatro):
     
     set_verdict(uno, socket.ntohl(pkg_hdr.contents.packet_id), NF_ACCEPT, len_recv, full_packet)
 
-    #tiene que retornar si o si un entero porque el HANDLER DICE QUE TIENE QUE RETORNALO
-    return 0
-
 
 c_handler = HANDLER(py_handler)
 
@@ -280,7 +347,7 @@ c_handler = HANDLER(py_handler)
 #Functions test
 ######################################################
 
-nfqh = nfq_open()
+nfqh = open_queue()
 unbind_pf(nfqh, socket.AF_INET)
 bind_pf(nfqh, socket.AF_INET)
 queue = create_queue(nfqh, 0, c_handler, None)
@@ -297,5 +364,5 @@ while 1:
     handle_packet(nfqh, recivido, 65535)
 
 destroy_queue(queue)
-nfq_close(nfqh)
+close_queue(nfqh)
 
